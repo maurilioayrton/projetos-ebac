218c0d528786b43714b2e3f617b9680d25bd9ba9
Maurilio Ayrton Teotonio Alves <maurilio.alves@mctic.gov.br>
2024-07-10 22:52:41 -0300

    adicionando arquivo md, e ajustando html, css e js

--- exercicio-html-js/main.js
+++ exercicio-html-js/main.js
@@ -1,1 +1,36 @@
-console.log("deu green");
+
+const formEbac = document.getElementById('form');
+
+formEbac.addEventListener('reset', function(event){
+    location.reload(); 
+})
+
+formEbac.addEventListener('submit', function(event){
+    event.preventDefault();     
+})   
+function validaValor(formEbac){
+        let campoa = document.getElementById("campoa").value;
+        let campob = document.getElementById("campob").value;
+        let form = document.getElementById("form");
+        if(campob > campoa){
+            mostraMsg(true);
+        }else{
+            mostraMsg(false);
+        }
+    }
+function mostraMsg(valida){
+    const mostraMsg = document.getElementById('msg');
+    
+    if(valida){
+        mostraMsg.textContent = `O Formulário está Válido, obrigado!`;
+        mostraMsg.className = 'msg true';
+    }else{
+        mostraMsg.textContent = `O Formulário não está Válido.. verifique se campo (B) é maior que o campo (A)`;
+        mostraMsg.className = 'msg false';
+    }
+    mostraMsg.style.display = 'block';
+    setTimeout(function() {
+        location.reload();
+    }, 10000); // 10000 milissegundos = 10 segundos
+}
+
